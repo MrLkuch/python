6270acc10cf90ba2f3c8f2b714bf4609412e707d
lkuch <kucheida.lucas@gmail.com>
2022-11-29 11:46:32 +0100

    Ajout d'un cours sur les variables
    Création du fichier de cours.
    et ajout de variables int et string.

---
+++ variables.py
@@ -0,0 +1,14 @@
+# l'opérateur d'affectation = permet d'injecter une valeur dans une variable.
+
+my_number1 = 123
+my_number2 = -42
+
+print(my_number1)
+print(my_number2)
+
+
+my_text1 = "Ceci est un string"
+my_text2 = 'la meme chose'
+
+print(my_text1)
+print(my_text2)
