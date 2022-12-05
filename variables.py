1e3502219c811e1e30ad55270ad1ffaace44767b
lkuch <kucheida.lucas@gmail.com>
2022-12-05 13:41:01 +0100

    Ajout de nouveaux type de variables
    Essaie de nouveaux type de variables, et leurs exemples.

--- variables.py
+++ variables.py
@@ -4,11 +4,70 @@ my_number1 = 123
 my_number2 = -42
 
 print(my_number1)
+print(type(my_number1))
+
 print(my_number2)
 
+# float
+
+my_number3 = 3.14
+my_number4 = -2.71
+# 0.0 ou 0. ou .0
+my_number5 = 0.0
+
+print(my_number3)
+print(type(my_number3))
+
+print(my_number4)
+print(my_number5)
+
+# string
 
 my_text1 = "Ceci est un string"
 my_text2 = 'la meme chose'
 
 print(my_text1)
 print(my_text2)
+
+#\ carac d'echapement
+#\ saut de ligne
+
+my_text3 ="abc\ndef"
+print(my_text3)
+print("\\")
+my_text4 = """abc
+def
+ghi"""
+print(my_text4)
+
+
+#bool
+
+my_boolean1 = True
+my_boolean2 = False
+print(my_boolean1)
+print(my_boolean2)
+print(type(my_boolean1))
+
+# none
+
+my_none = None
+print(my_none)
+print(type(my_none))
+
+
+#permutation de variable
+a=123
+b=42
+
+a, b = b, a
+
+print(a)
+
+c=0
+c=a
+a=b
+b=c
+
+print(a)
+
